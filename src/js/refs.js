0d32a275981d88532fea0d1786cf8f9a18f35c81
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-06-19 13:52:02 +0300

    add contacts nav-link

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,23 +1,9 @@
 const refs = {
-  // phoneBtn: document.querySelector('.js-phone-btn'),
-  // phoneBtnLink: document.querySelector('.js-phone-btn-link'),
-  // consultationBtn: document.querySelectorAll('.js-consultation-btn'),
-  // joinUsModalWin: document.querySelector('.js-join-us-modal-win'),
-  // closeModalWinBtn: document.querySelector('.js-close-modal-win-btn'),
-  // heroJoinUsBtn: document.querySelector('.js-hero-join-us-btn'),
-  // vacanciesList: document.querySelector('.js-vacancies-list'),
   questionsList: document.querySelector('.js-questions-list'),
-  // showMoreBtn: document.querySelectorAll('.js-show-more-btn'),
   mobileMenuToggleBtn: document.querySelectorAll('.js-mobile-menu-toggle'),
   mobileMenu: document.querySelector('.js-mobile-menu'),
   mobileMenuList: document.querySelector('.js-mobile-menu-links-list'),
-  // nameInput: document.querySelectorAll('.js-join-us-form-name-input'),
-  // phoneInput: document.querySelectorAll('.js-join-us-form-phone-input'),
-  // supportBnt: document.querySelector('.js-support-bnt'),
-  // supportMenu: document.querySelector('.js-support-menu'),
-  // counter: document.querySelectorAll('.js-statistics-value-wrap'),
-  // statisticsList: document.querySelector('.js-statistics-list'),
-  // mainNavigation: document.querySelectorAll('.js-main-navigation'),
+  mainNavigation: document.querySelectorAll('.js-main-navigation'),
 };
 
 export default refs;
