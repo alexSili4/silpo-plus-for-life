dcba890afe8b33456227e8ee9ce13d4108b374f0
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-07-01 09:12:19 +0300

    add how-to-support-bank

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,42 @@
 import { defineConfig } from 'vite';
+import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
   root: 'src',
   build: {
+    rollupOptions: {
+      input: glob.sync('./src/*.html'),
+      output: {
+        // Настройка для вывода файлов в разные папки
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith('.css')) {
+            return 'css/styles[extname]';
+          }
+          if (assetInfo.name.endsWith('.js')) {
+            return 'js/script[extname]';
+          }
+          if (assetInfo.name.endsWith('.svg')) {
+            return 'icons/[name]-[hash][extname]';
+          }
+          if (
+            assetInfo.name.endsWith('.ttf') ||
+            assetInfo.name.endsWith('.otf')
+          ) {
+            return 'fonts/[name]-[hash][extname]';
+          }
+          if (assetInfo.name.endsWith('.png')) {
+            return 'images/[name]-[hash][extname]';
+          }
+          // Настройка для других файлов, если необходимо
+          return 'assets/[name]-[hash][extname]';
+        },
+        // Настройка для вывода файлов JavaScript
+        chunkFileNames: 'js/script.js',
+        entryFileNames: 'js/script.js',
+      },
+    },
     outDir: '../dist',
   },
   plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
