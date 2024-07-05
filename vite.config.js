1e6c0c1f05431952ffb56001a116886b154d398e
Silich Alexander <124524007+sili41995@users.noreply.github.com>
2024-07-05 20:11:53 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,44 +9,44 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, './src/index.html'),
-        // donation: resolve(__dirname, './src/donation/index.html'),
-        // thanks: resolve(__dirname, './src/thanks/index.html'),
-        // problem: resolve(__dirname, './src/problem/index.html'),
+        donation: resolve(__dirname, './src/donation/index.html'),
+        thanks: resolve(__dirname, './src/thanks/index.html'),
+        problem: resolve(__dirname, './src/problem/index.html'),
       },
       // input: glob.sync('./src/*.html'),
       output: {
         // Настройка для вывода файлов в разные папки
         assetFileNames: (assetInfo) => {
           if (assetInfo.name.endsWith('.css')) {
-            return 'dist/css/styles-[hash][extname]';
+            return 'css/styles-[hash][extname]';
           }
           if (assetInfo.name.endsWith('.js')) {
-            return 'dist/js/script-[hash][extname]';
+            return 'js/script-[hash][extname]';
           }
           if (assetInfo.name.endsWith('.svg')) {
-            return 'dist/icons/[name]-[hash][extname]';
+            return 'icons/[name]-[hash][extname]';
           }
           if (
             assetInfo.name.endsWith('.ttf') ||
             assetInfo.name.endsWith('.otf')
           ) {
-            return 'dist/fonts/[name]-[hash][extname]';
+            return 'fonts/[name]-[hash][extname]';
           }
           if (
             assetInfo.name.endsWith('.png') ||
             assetInfo.name.endsWith('.jpg')
           ) {
-            return 'dist/images/[name]-[hash][extname]';
+            return 'images/[name]-[hash][extname]';
           }
           if (assetInfo.name.endsWith('.mp4')) {
-            return 'dist/video/[name]-[hash][extname]';
+            return 'video/[name]-[hash][extname]';
           }
           // Настройка для других файлов, если необходимо
-          return 'dist/assets/[name]-[hash][extname]';
+          return 'assets/[name]-[hash][extname]';
         },
         // Настройка для вывода файлов JavaScript
-        chunkFileNames: 'dist/js/script-[hash].js',
-        entryFileNames: 'dist/js/script-[hash].js',
+        chunkFileNames: 'js/script-[hash].js',
+        entryFileNames: 'js/script-[hash].js',
       },
     },
     outDir: '../dist',
